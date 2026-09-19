5b2cdb338b61ce84563ecd41d18c91ae3bc4087f
pradeepMorya77 <psir370@gmail.com>
2026-09-19 23:24:43 +0530

    Delete Chapter 3/o1_Intro_to_Strings.py

--- Chapter 3/o1_Intro_to_Strings.py
+++
@@ -1,6 +0,0 @@
-name = 'Pradeep' # single quoted string
-name = "Pradeep" # double quoted string
-name = '''Pradeep''' # triple quoted string
-
-nameshort = name[0:4] # first index is include and last index is not include
-print(nameshort)
