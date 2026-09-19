fe19df1bf6fe467a12c2c832517ebba4df15edd0
pradeepMorya77 <psir370@gmail.com>
2026-09-19 23:23:37 +0530

    Add files via upload

---
+++ Chapter 3/02_Str_functions.py
@@ -0,0 +1,13 @@
+name = "Pradeep Morya"
+print(f"The lenght of {name} is {len(name)}")
+print(f"The string {name} is endswith Morya is {name.endswith("Morya")}")
+print(f"The string {name} is startwith Morya is {name.startswith("Morya")}")
+print(f"The string is {name} is Capitalizes is {name.capitalize()}")
+
+print(f"The string is {name} is find at {name.find("Pradeep")}")
+
+print(f"The string is {name} is replaced with {name.replace("Morya", "Morya Kahar")}")
+
+print(f"The string {name} is uppercase is {name.upper()}")
+print(f"The string {name} is lowercase is {name.lower()}")
+print(f"The string {name} is digit is {name.isdigit()}")
---
+++ Chapter 3/03_Escape_Sequence.py
@@ -0,0 +1,11 @@
+str = "Pradeep is a good boy \nbut not a bad boy"
+print(str)
+
+str = "Pradeep is a good boy but not a bad \"boy\""
+print(str)
+
+str = "Pradeep is a good boy but not a bad \\ boy"
+print(str)
+
+str = "Pradeep is a good boy but not a bad \tboy"
+print(str)
