1af45257aeb0af9e55191a848e032dc31a8d43de
pradeepMorya77 <psir370@gmail.com>
2026-09-19 22:42:53 +0530

    Create o1_Intro_to_Strings.py

---
+++ Chapter 3/o1_Intro_to_Strings.py
@@ -0,0 +1,6 @@
+name = 'Pradeep' # single quoted string
+name = "Pradeep" # double quoted string
+name = '''Pradeep''' # triple quoted string
+
+nameshort = name[0:4] # first index is include and last index is not include
+print(nameshort)
